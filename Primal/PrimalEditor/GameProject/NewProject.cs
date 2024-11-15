d84fb3c8bbe1bc9ba2e57e6e33bc1ab91f8b89b4
gydnjs <khw33352182@gmail.com>
2024-11-15 18:55:33 +0900

    Create window with wpf
    create start window

---
+++ Primal/PrimalEditor/GameProject/NewProject.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimalEditor.GameProject
+{
+    class NewProject : ViewModelBase
+    {
+        private string _name = "NewProject";
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                if (_name != value)
+                {
+                    _name = value;
+                    OnPropertyChanged(nameof(Name));
+                }
+            }
+        }
+    }
+}
